5bf7abf70c9d2b604ff02caed4b1f92dfedb229f
Lewi1006 <coach.wind11@gmail.com>
2026-06-23 21:11:17 +0200

    bug: img doesnt show on canvas

--- models/world.class.js
+++ models/world.class.js
@@ -1,19 +1,28 @@
-class World{
-    character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-    ctx;
+class World {
+  character = new Character();
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  ctx;
+  canvas;
+  
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
 
-    constructor(canvas){
-        this.ctx = canvas.getContext("2D");
-        this.draw();
-    }
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height,
+    );
 
-    draw(){
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-    };
-
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }
