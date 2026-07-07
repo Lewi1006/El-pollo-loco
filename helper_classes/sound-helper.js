4142e118ce3f44fa2b1361d2513c0cbf75cea2e7
Lewi1006 <coach.wind11@gmail.com>
2026-07-07 22:24:45 +0200

    feat: soundhub and sounds added

---
+++ helper_classes/sound-helper.js
@@ -0,0 +1,27 @@
+class SoundHub {
+    // Audio
+    static jump = new Audio(...);
+
+    // Array containing all audio files
+    static allSounds = [SoundHub.piano, SoundHub.guitar, SoundHub.drums];
+
+
+    // plays a single audio file
+    static playOne(sound, instrumentId) {  
+        sound.volume = 0.2; 
+        sound.currentTime = 0; 
+        sound.play();  
+    }
+
+    // pauses all audio files
+    static pauseAll() {
+        SoundHub.allSounds.forEach(sound => {
+            sound.pause();  
+        });
+    }
+
+    //pauses a single audio file
+    static pauseOne(sound, instrumentId) {
+        sound.pause();  
+    }
+}
