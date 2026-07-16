604a1e6975b76163fe786947b5bd3e79fce2cf6b
Lewi1006 <coach.wind11@gmail.com>
2026-07-17 00:55:21 +0200

    feat: chicken

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,13 +1,41 @@
 import { ImageHelper } from "../helper_classes/image-helper.js";
 import { MovableObject } from "./movable-object.class.js";
 
-export class BackgroundObject extends MovableObject{
+/**
+ * Represents a background layer object in the game world.
+ *
+ * BackgroundObject extends MovableObject and is used to display the different
+ * background layers of a level. Each object loads one image layer and places
+ * it at the given x-position to create the scrolling background effect.
+ *
+ * The objects are positioned at the bottom of the canvas by calculating their
+ * y-position based on the canvas height and the object's height.
+ *
+ * @class
+ */
+export class BackgroundObject extends MovableObject {
     // #region properties
+    /**
+     * @property {number} width - Width of the background image.
+     * @property {number} height - Height of the background image.
+     */
     width = 720;
     height = 480;
     // #endregion
 
-    constructor(indexLayers, x){
+    /**
+     * Creates a new BackgroundObject instance.
+     *
+     * Loads the selected background layer image and places it at the given
+     * horizontal position. Multiple BackgroundObjects with different layers and
+     * positions are used together to create the complete level background.
+     *
+     * @constructor
+     *
+     * @param {number} indexLayers - Index of the background layer image to load.
+     * @param {number} x - Horizontal position where the background layer is placed.
+     */
+    constructor(indexLayers, x) {
         super();
         this.loadImage(ImageHelper.BACKGROUND.layers[indexLayers]);
         this.x = x;
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,17 +3,49 @@ import { ImageHelper } from "../helper_classes/image-helper.js";
 import { IntervalHub } from "../helper_classes/interval-helper.js";
 import { SoundHub } from "../helper_classes/sound-helper.js";
 
+/**
+ * Represents a chicken enemy in the game.
+ *
+ * Chicken extends MovableObject and uses inherited movement, gravity,
+ * collision, health, and animation functionality. The chicken enemy can walk,
+ * be damaged, and enter a dead state.
+ *
+ * The class provides the image sets required for the walking and death
+ * animations.
+ *
+ * @class Chicken
+ */
 export class Chicken extends MovableObject {
     // #region properties
+    /**
+     * @property {number} y - Vertical position of the chicken.
+     * @property {number} height - Height of the chicken image.
+     * @property {number} width - Width of the chicken image.
+     * @property {string[]} imagesWalk - Image paths used for the walking animation.
+     * @property {string[]} imagesDead - Image paths used for the death animation.
+     */
     y = 350;
     height = 70;
     width = 80;
     imagesWalk = ImageHelper.CHICKEN.walk;
     imagesDead = ImageHelper.CHICKEN.dead;
-    // deathTime = 0;
 
     // #endregion
 
+    /**
+     * Creates a new Chicken enemy.
+     *
+     * The constructor initializes the chicken's images, assigns a random starting
+     * position and movement speed, and starts the movement and animation updates.
+     *
+     * The random x-position allows chickens to spawn at different locations in
+     * the level, while the random speed creates variation between enemies.
+     *
+     * The update intervals continuously handle the chicken's movement and
+     * animation during the game.
+     *
+     * @constructor
+     */
     constructor() {
         super();
         this.loadImage(this.imagesWalk[0]);
@@ -28,17 +60,38 @@ export class Chicken extends MovableObject {
 
     // #region methods
 
+    /**
+     * Starts the chicken movement and animation updates.
+     *
+     * Calls the movement and animation methods which are continuously updated
+     * through intervals started in the constructor.
+     *
+     * @returns {void}
+     */
     animate() {
         this.updateMovement();
         this.updateAnimation();
     }
 
+    /**
+     * Moves the chicken to the left while the game is active.
+     *
+     * @returns {void}
+     */
     updateMovement = () => {
         if (!this.world || !this.world.gameStarted) return;
 
         this.moveLeft();
     };
 
+    /**
+     * Updates the chicken animation depending on its state.
+     *
+     * Plays the death animation when the chicken is dead, otherwise the walking
+     * animation is played.
+     *
+     * @returns {void}
+     */
     updateAnimation = () => {
         if (!this.world || !this.world.gameStarted) return;
         if (this.isDead()) {
@@ -48,8 +101,15 @@ export class Chicken extends MovableObject {
         }
     };
 
-
-      die(){
+    /**
+     * Handles the chicken death state.
+     *
+     * Calls the parent die() method to apply the default death logic and then
+     * plays the chicken-specific death sound effect.
+     *
+     * @returns {void}
+     */
+    die() {
         super.die();
         SoundHub.playOne(SoundHub.chickenDead, 0.6);
     }
