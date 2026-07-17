755bff62451e3feea5017666b494471e79dd10e3
Lewi1006 <coach.wind11@gmail.com>
2026-07-17 09:32:12 +0200

    feat: bottle status jsdoc

--- models/status-bar-bottles.class.js
+++ models/status-bar-bottles.class.js
@@ -1,10 +1,31 @@
 import { ImageHelper } from "../helper_classes/image-helper.js";
 import { DrawableObject } from "./drawable-object.class.js";
 
+/**
+ * Represents the bottle status bar displayed in the game.
+ *
+ * BottleStatus extends DrawableObject and displays the player's remaining
+ * throwable bottles. The displayed image changes depending on the current
+ * percentage value.
+ *
+ * @class
+ */
 export class BottleStatus extends DrawableObject {
+    /**
+     * @property {string[]} imagesStatusBottle - Image paths used for the different status bar states.
+     * @property {number} percentage - Current bottle amount represented as a percentage.
+     */
     imagesStatusBottle = ImageHelper.STATUSBAR.bottle;
     percentage = 0;
 
+    /**
+     * Creates a new bottle status bar.
+     *
+     * Loads all status bar images, sets the position and size of the status bar,
+     * and initializes the displayed image based on the starting percentage.
+     *
+     * @constructor
+     */
     constructor() {
         super();
         this.loadImages(this.imagesStatusBottle);
@@ -15,6 +36,16 @@ export class BottleStatus extends DrawableObject {
         this.setPercentage(this.percentage);
     }
 
+    /**
+     * Updates the bottle status bar percentage and changes the displayed image.
+     *
+     * Calculates the correct image index based on the given percentage and loads
+     * the matching image from the image cache.
+     *
+     * @param {number} percentage - Current bottle amount as a percentage value.
+     *
+     * @returns {void}
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let index = this.resolveImageIndex(percentage);
@@ -22,8 +53,18 @@ export class BottleStatus extends DrawableObject {
         this.img = this.imageCache[path];
     }
 
-        resolveImageIndex(percentage) {
-       if (this.percentage >= 100) {
+    /**
+     * Determines which status bar image should be displayed.
+     *
+     * The percentage value is mapped to one of the available status images:
+     * 100%, 80%, 60%, 40%, 12.5%, or 0%.
+     *
+     * @param {number} percentage - Current bottle amount as a percentage value.
+     *
+     * @returns {number} Index of the matching status bar image.
+     */
+    resolveImageIndex(percentage) {
+        if (this.percentage >= 100) {
             return 5;
         } else if (this.percentage >= 80) {
             return 4;
@@ -31,7 +72,7 @@ export class BottleStatus extends DrawableObject {
             return 3;
         } else if (this.percentage >= 40) {
             return 2;
-        } else if (this.percentage >= 20) {
+        } else if (this.percentage >= 12.5) {
             return 1;
         } else {
             return 0;
